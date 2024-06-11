8e34e243bbcd0e17944ee09c8a5ac13384892ee0
Michael Hinton <github@hinton.e4ward.com>
2024-06-11 01:26:17 -0600

    WIP: Try to write midi file to USB MIDI interface

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::fs;
 
 // External imports
 use anyhow::Result;
+use hidapi_rusb::HidApi;
 use midly::{num, MetaMessage, Smf, TrackEventKind};
 
 // Internal imports
@@ -22,7 +23,38 @@ fn main() -> Result<()> {
     let _executable = &args[0];
     let file = &args[1];
 
-    print_meta(file)
+    print_meta(file)?;
+    send_midi_file(file)?;
+    Ok(())
+}
+
+/// Send the contents of a midi file to a USB midi interface
+fn send_midi_file(file: &str) -> Result<()> {
+    // Get the USB Midi interface
+    let api = HidApi::new().unwrap();
+    // Print out information about all connected devices
+    for device in api.device_list() {
+        println!("{:#X?}", device);
+    }
+
+    println!("Got here");
+
+    // Connect to device using its VID and PID
+    // For now, hard code to IDs for my "USB MIDI Cable"
+    // let (vid, pid) = (0x46D, 0xC02C);
+    let (vid, pid) = (0xFC02, 0x0101);
+    let device = api.open(vid, pid)?;
+
+    // // Read data from device
+    // let mut buf = [0u8; 8];
+    // let res = device.read(&mut buf[..])?;
+    // println!("Read: {:?}", &buf[..res]);
+
+    // Write data to device
+    let bytes = fs::read(file)?;
+    let res = device.write(&bytes[..])?;
+    println!("Wrote: {:?} bytes to {}", res, file);
+    Ok(())
 }
 
 /// Print out the meta data of a midi file and their tick timestamp while
