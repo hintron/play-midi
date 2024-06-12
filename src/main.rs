8edec93f5363112192feab7a1c887cb6c4b37eec
Michael Hinton <github@hinton.e4ward.com>
2024-06-12 11:26:39 -0600

    Convert play_note from a closure to a function

--- src/main.rs
+++ src/main.rs
@@ -190,33 +190,29 @@ fn open_midi_usb() -> Result<MidiOutputConnection> {
 }
 
 fn test_play(conn_out: &mut MidiOutputConnection) -> Result<()> {
-    {
-        // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
-        let mut play_note = |note: u8, duration: u64| {
-            const NOTE_ON_MSG: u8 = 0x90;
-            const NOTE_OFF_MSG: u8 = 0x80;
-            const VELOCITY: u8 = 0x64;
-            // We're ignoring errors in here
-            println!("Playing note {note}");
-            let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
-            sleep(Duration::from_millis(duration * 150));
-            let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
-        };
-
-        sleep(Duration::from_millis(4 * 150));
-        println!("Playing test song...");
-
-        play_note(66, 4);
-        play_note(65, 3);
-        play_note(63, 1);
-        play_note(61, 6);
-        play_note(59, 2);
-        play_note(58, 4);
-        play_note(56, 4);
-        play_note(54, 4);
-    }
+    sleep(Duration::from_millis(4 * 150));
+    println!("Playing test song...");
+    play_note(conn_out, 66, 4);
+    play_note(conn_out, 65, 3);
+    play_note(conn_out, 63, 1);
+    play_note(conn_out, 61, 6);
+    play_note(conn_out, 59, 2);
+    play_note(conn_out, 58, 4);
+    play_note(conn_out, 56, 4);
+    play_note(conn_out, 54, 4);
     sleep(Duration::from_millis(150));
     Ok(())
+}
+
+fn play_note(conn_out: &mut MidiOutputConnection, note: u8, duration: u64) {
+    const NOTE_ON_MSG: u8 = 0x90;
+    const NOTE_OFF_MSG: u8 = 0x80;
+    const VELOCITY: u8 = 0x64;
+    // We're ignoring errors in here
+    println!("Playing note {note}");
+    let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
+    sleep(Duration::from_millis(duration * 150));
+    let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
 }
 
 fn play_midi_file(conn_out: &mut MidiOutputConnection, file: &str) -> Result<()> {
